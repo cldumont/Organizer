2361114acf2496134cc4178c858ac7ac1cfdd551
Craig Dumont <cldumont77@comcast.net>
2019-02-09 13:07:14 -0500

    Initial Commit

---
+++ Organizer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Organizer
+//
+//  Created by Craig Dumont on 2/9/19.
+//  Copyright © 2019 Craig Dumont. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
